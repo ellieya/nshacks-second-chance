dfdf25a6a9cecb67d660420282867d933ac3194e
Ellie <j.chen.ellie@gmail.com>
2019-11-16 10:57:21 -0500

    Last push

--- src/pages/SupportersHome.js
+++ src/pages/SupportersHome.js
@@ -9,7 +9,7 @@ class SupportersHome extends React.Component {
             <div className="supporters-home">
                 <span className="grayed-italics">Supporters Homepage</span>
                 <PointsDisplay/>
-                <button className="big-button">Your Volunteer Cards</button>
+                <a href="https://drive.google.com/file/d/1l7ME7os5JeGDgTv3DXr2lQLR2RZB9h11/view?usp=sharing"><button className="big-button">Your Volunteer Cards</button></a>
                 <button className="big-button" onClick={() => this.props.action("SupportersRewards")}>Redeem Points</button>
                 <button className="big-button" onClick={() => this.props.action("SupportersHelp")}>Help</button>
             </div>
